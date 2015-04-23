c0af5e1f529d4c234300cc52b6ed58b124563e70
TomasKunovsky <tkunovsky@seznam.cz>
2015-04-23 09:44:27 +0200

    2 tridy datove vrstvy

---
+++ PIS/src/pis/data/Course.java
@@ -0,0 +1,64 @@
+package pis.data;
+
+import java.util.Collection;
+import java.util.Vector;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.OneToMany;
+
+import static javax.persistence.FetchType.EAGER;
+import static javax.persistence.CascadeType.ALL;
+
+@Entity
+@Table(name = "course")
+public class Course {
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private long id;
+	private int price;
+	private String description;
+	private String name;
+	@OneToMany(mappedBy = "course", orphanRemoval = true, fetch = EAGER, cascade = ALL)
+	private Collection<OpenCourse> openCourses;
+	
+	public Course() {
+		openCourses = new Vector<OpenCourse>();
+	}
+	
+	public Collection<OpenCourse> getOpenCourses() {
+		return openCourses;
+	}
+	public void setOpenCourses(Collection<OpenCourse> openCourses) {
+		this.openCourses = openCourses;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ PIS/src/pis/data/OpenCourse.java
@@ -0,0 +1,67 @@
+package pis.data;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import static javax.persistence.FetchType.EAGER;
+import javax.persistence.Temporal;
+import static javax.persistence.TemporalType.DATE;
+
+@Entity
+@Table(name = "open_course")
+public class OpenCourse {
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private long id;
+	private String place;
+	private int capacity;
+	@Temporal(DATE)
+	private Date startDate;
+	@Temporal(DATE)
+	private Date endDate;
+	@ManyToOne(fetch = EAGER)
+	private Course course;
+	
+	public Course getCourse() {
+		return course;
+	}
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+}
